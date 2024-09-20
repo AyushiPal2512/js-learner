8d5f008a5f19b2d79d42eceedd562e4ab501267a
AyushiPal2512 <ayushipalrtm@gmail.com>
2024-09-20 14:30:13 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "ayushi@google.com"
+var accountPassward = "12345"
+accountCity = "jaipur"
+
+//  accountId = 2 // not allowed
+
+console.log(accountId);
